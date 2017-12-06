6662ccc8cfb4a3df890814facaf2f1e12d6014ba
Jesus_Crie <lucas.malandrino@gmail.com>
2017-12-06 17:40:40 +0100

    Renamed resource files

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ def start(day: Day):
 
 
 if __name__ == "__main__":
-	start(Day1(file = "resources/day1.bbw"))
-	start(Day2(file = "resources/day2.bbw"))
+	start(Day1(file = "resources/day1.input"))
+	start(Day2(file = "resources/day2.input"))
 	start(Day3(inp = "312051"))
-	start(Day4(file = "resources/day4.bbw"))
-	start(Day5(file = "resources/day5.bbw"))
+	start(Day4(file = "resources/day4.input"))
+	start(Day5(file = "resources/day5.input"))
 	start(Day6(inp = "14 0 15 12 11 11 3 5 1 6 8 4 9 1 8 4"))
