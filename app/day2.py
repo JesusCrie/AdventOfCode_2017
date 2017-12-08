9dc2a65a6148ee1a916cf22a2ea59c27b478aef7
Jesus_Crie <lucas.malandrino@gmail.com>
2017-12-08 21:17:32 +0100

    Day 2, part 2

--- app/day2.py
+++ app/day2.py
@@ -8,4 +8,19 @@ class Day2(Day):
 		print(sum([max(row) - min(row) for row in rows]))
 
 	def part_2(self):
-		pass
+		rows = [self.to_int(s.split("\t")) for s in self.input_2.splitlines()]
+
+		total = 0
+		for row in rows:
+			for num in row:
+				for other in row:
+					if num == other:
+						continue
+					elif self.is_divisible(num, other):
+						total += int(num / other)
+
+		print(total)
+
+	@staticmethod
+	def is_divisible(first: int, second: int):
+		return first % second == 0
