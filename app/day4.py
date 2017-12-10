4f9cc2ccacb0ec41a947ba75bf3715e4955c33e7
Jesus_Crie <lucas.malandrino@gmail.com>
2017-12-10 14:59:07 +0100

    Day 4.2

--- app/day4.py
+++ app/day4.py
@@ -8,8 +8,29 @@ class Day4(Day):
 		print(len([l for l in lines if self.is_line_valid(l)]))
 
 	def part_2(self):
-		pass
+		lines = [l.split() for l in self.input_2.splitlines()]
+		valid = 0
+		for line in lines:
+			try:
+				for i, word in enumerate(line):
+					anagrams = [o for o in line[i + 1:] if self.is_anagram(word, o)]
+					if len(anagrams) > 0:
+						raise StopIteration
+				valid += 1
+			except StopIteration:
+				pass
+
+		print(valid)
 
 	@staticmethod
 	def is_line_valid(line: list):
 		return len(line) == len(set(line))
+
+	@staticmethod
+	def is_anagram(first: str, second: str):
+		list_first = list(first)
+		list_second = list(second)
+		list_first.sort()
+		list_second.sort()
+
+		return list_first == list_second
