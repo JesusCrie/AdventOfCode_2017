f741fe6679c7a61ac78949379a66e8acc423a805
Jesus_Crie <lucas.malandrino@gmail.com>
2017-12-10 16:07:56 +0100

    Giveup day 6.2...

--- app/day6.py
+++ app/day6.py
@@ -23,7 +23,24 @@ class Day6(Day):
 		print(len(configs))
 
 	def part_2(self):
-		pass
+		banks = self.to_int(self.input.split())
+		configs = list()
+		configs.append(self.banks_to_str(banks))
+
+		while True:
+			bank_index = banks.index(max(banks))
+			amount = banks[bank_index]
+			banks[bank_index] = 0
+
+			self.dispatch_from_index(banks, bank_index + 1, amount)
+
+			str_bank = self.banks_to_str(banks)
+			if str_bank in configs:
+				break
+			configs.append(str_bank)
+
+		print(len(configs))
+		# Give up, need to use https://en.wikipedia.org/wiki/Cycle_detection#Floyd.27s_Tortoise_and_Hare
 
 	@staticmethod
 	def dispatch_from_index(banks: list, index: int, amount: int):
--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from app.day5 import Day5
 from app.day6 import Day6
 
 
-def start(day: Day):
+def start(day: Day, skip_1: bool = False, skip_2: bool = False):
 	print(">>> " + day.__class__.__name__ + " <<<")
 	print("> Part 1")
-	day.part_1()
+	day.part_1() if not skip_1 else print("SKIPPED")
 	print("> Part 2")
-	day.part_2()
+	day.part_2() if not skip_2 else print("SKIPPED")
 
 
 if __name__ == "__main__":
@@ -20,5 +20,5 @@ if __name__ == "__main__":
 	start(Day2(file = "resources/day2.input"))
 	start(Day3(inp = "312051"))
 	start(Day4(file = "resources/day4.input"))
-	start(Day5(file = "resources/day5.input"))
-	start(Day6(inp = "14 0 15 12 11 11 3 5 1 6 8 4 9 1 8 4"))
+	start(Day5(file = "resources/day5.input"), skip_2 = True)
+	start(Day6(inp = "14 0 15 12 11 11 3 5 1 6 8 4 9 1 8 4"), skip_1 = True)
